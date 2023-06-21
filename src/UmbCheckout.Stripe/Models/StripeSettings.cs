fe3f047b332e20b61db544f8b5ae64527c2dbd1c
Aaron <aaron@umbhost.net>
2023-06-21 19:25:19 +0100

    Removed local Stripe

--- src/UmbCheckout.Stripe/Models/StripeSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UmbCheckout.Stripe.Models
-{
-    public class StripeSettings
-    {
-        public string WebHookSecret { get; init; } = string.Empty;
-
-        public string ApiKey { get; init; } = string.Empty;
-    }
-}
