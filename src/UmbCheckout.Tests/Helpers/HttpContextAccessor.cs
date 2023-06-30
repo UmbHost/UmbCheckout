a667f84bbf8bda5d1656c70c4132190386fb6520
Aaron <aaron@umbhost.net>
2023-06-30 20:13:58 +0100

    Session service tests

---
+++ src/UmbCheckout.Tests/Helpers/HttpContextAccessor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Session;
+using Moq;
+
+namespace UmbCheckout.Tests.Helpers
+{
+    internal class HttpContextAccessor
+    {
+        public static Microsoft.AspNetCore.Http.HttpContextAccessor GetHttpContext(string incomingRequestUrl, string host)
+        {
+            var context = new DefaultHttpContext();
+            var session = new Mock<ISession>();
+            context.Request.Path = incomingRequestUrl;
+            context.Request.Host = new HostString(host);
+            context.Session = session.Object;
+            //Do your thing here...
+
+            var obj = new Microsoft.AspNetCore.Http.HttpContextAccessor();
+            obj.HttpContext = context;
+            return obj;
+        }
+    }
+}
