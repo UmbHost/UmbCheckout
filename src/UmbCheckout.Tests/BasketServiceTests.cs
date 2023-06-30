1a4d89b5b11857d5491eb9b937b786d271dbb4e9
Aaron <aaron@umbhost.net>
2023-06-30 18:17:02 +0100

    More Basket tests

--- src/UmbCheckout.Tests/BasketServiceTests.cs
+++ src/UmbCheckout.Tests/BasketServiceTests.cs
@@ -25,7 +25,7 @@ public class BasketServiceTests
         var checkoutSession = new UmbCheckoutSession();
         sessionService.Setup(x => x.Get()).ReturnsAsync(checkoutSession);
         sessionService.Setup(x => x.Update(It.IsAny<Basket>())).ReturnsAsync(checkoutSession);
-        
+
         var mockScope = new Mock<IScope>();
         mockScope.Setup(x => x.Notifications).Returns(new Mock<IScopedNotificationPublisher>().Object);
         coreScopeProvider.Setup(x => x.CreateCoreScope(IsolationLevel.Unspecified, RepositoryCacheMode.Unspecified, null, null, null, false, true))
@@ -102,6 +102,106 @@ public class BasketServiceTests
 
         Assert.NotNull(reducedQuantityResult);
         Assert.Contains(result.LineItems, x => x.Id.Equals(product.Id) && x.Quantity == 1);
+    }
+
+    [Fact]
+    public async void RemoveProductFromBasket()
+    {
+        // Arrange
+        var product = new LineItem
+        {
+            Id = Guid.NewGuid(),
+            Quantity = 2,
+            Name = "Test Product",
+            Price = 10.00m
+        };
+
+        // Act
+        var result = await _basketService.Add(product);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Contains(result.LineItems, x => x.Id.Equals(product.Id));
+
+        var removedResult = await _basketService.Remove(product.Id);
+
+        Assert.NotNull(removedResult);
+        Assert.DoesNotContain(result.LineItems, x => x.Id.Equals(product.Id));
+    }
+
+    [Fact]
+    public async void ClearBasket()
+    {
+        // Arrange
+        var product = new LineItem
+        {
+            Id = Guid.NewGuid(),
+            Quantity = 2,
+            Name = "Test Product",
+            Price = 10.00m
+        };
+
+        // Act
+        var result = await _basketService.Add(product);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Contains(result.LineItems, x => x.Id.Equals(product.Id));
+
+        var clearedBasket = await _basketService.Clear();
+        Assert.NotNull(clearedBasket);
+        Assert.False(clearedBasket.LineItems.Any());
+    }
+
+    [Fact]
+    public async void TotalItemsInBasket()
+    {
+        // Arrange
+        var product = new LineItem
+        {
+            Id = Guid.NewGuid(),
+            Quantity = 2,
+            Name = "Test Product",
+            Price = 10.00m
+        };
+
+        // Act
+        var result = await _basketService.Add(product);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Contains(result.LineItems, x => x.Id.Equals(product.Id));
+
+        Assert.True(result.ItemCount == 1);
+    }
+
+    [Fact]
+    public async void TotalBasketAmount()
+    {
+        var products = new List<LineItem>
+        {
+            new()
+            {
+                Id = Guid.NewGuid(),
+                Quantity = 2,
+                Name = "Test Product",
+                Price = 10.00m
+            },
+            new()
+            {
+                Id = Guid.NewGuid(),
+                Quantity = 1,
+                Name = "Test Product 2",
+                Price = 20.00m
+            }
+        };
+
+        // Act
+        var result = await _basketService.Add(products);
+        var subTotal = await _basketService.SubTotal();
+        // Assert
+        Assert.NotNull(result);
+        Assert.True(subTotal == 40.00m);
     }
 
     [Fact]
