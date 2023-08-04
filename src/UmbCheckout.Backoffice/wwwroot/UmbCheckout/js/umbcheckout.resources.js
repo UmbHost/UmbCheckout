8ff71a7bddc37ee5acafd09b1a0ee0e05ed8f1c1
Aaron <aaron@umbhost.net>
2023-08-04 11:30:43 +0100

    Moved stripe resources

--- src/UmbCheckout.Backoffice/wwwroot/UmbCheckout/js/umbcheckout.resources.js
+++ src/UmbCheckout.Backoffice/wwwroot/UmbCheckout/js/umbcheckout.resources.js
@@ -31,54 +31,6 @@
                     return response;
                 }
                 );
-        },
-        getShippingRates: function () {
-
-            return $http.get("backoffice/UmbCheckout/ShippingRatesApi/GetShippingRates")
-                .then(function (response) {
-                    return response;
-                }
-                );
-        },
-        getStripeShippingRates: function () {
-
-            return $http.get("backoffice/UmbCheckout/ShippingRatesApi/GetStripeShippingRates")
-                .then(function (response) {
-                    return response;
-                }
-                );
-        },
-        getShippingRate: function (id) {
-
-            return $http.get("backoffice/UmbCheckout/ShippingRatesApi/GetShippingRate?id=" + id)
-                .then(function (response) {
-                    return response;
-                }
-                );
-        },
-        getStripeShippingRate: function (id) {
-
-            return $http.get("backoffice/UmbCheckout/ShippingRatesApi/GetStripeShippingRate?id=" + id)
-                .then(function (response) {
-                    return response;
-                }
-                );
-        },
-        updateShippingRate: function (configurationValues, id) {
-            configurationValues.id = id
-            return $http.patch("backoffice/UmbCheckout/ShippingRatesApi/UpdateShippingRate", configurationValues)
-                .then(function (response) {
-                    return response;
-                }
-                );
-        },
-        deleteShippingRate: function (id) {
-
-            return $http.delete("backoffice/UmbCheckout/ShippingRatesApi/DeleteShippingRate?id=" + id)
-                .then(function (response) {
-                    return response;
-                }
-                );
-        },
+        }
     }
 });
