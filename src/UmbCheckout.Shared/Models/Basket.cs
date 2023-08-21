f0e0e1a759dae22d86071f6c5afe9cf4d41cc195
Aaron <aaron@umbhost.net>
2023-08-21 13:06:45 +0100

    Wired up basket total

--- src/UmbCheckout.Shared/Models/Basket.cs
+++ src/UmbCheckout.Shared/Models/Basket.cs
@@ -5,6 +5,7 @@
     /// </summary>
     public class Basket
     {
+        private decimal _total;
         public string Id { get; set; } = string.Empty;
 
         public string SessionId { get; set; } = string.Empty;
@@ -13,6 +14,19 @@
 
         public long ItemCount => LineItems.Count();
 
-        public decimal Total { get; set; }
+        public decimal Total
+        {
+            get
+            {
+                if (_total == default)
+                {
+                    _total = LineItems.Select(x => x.Price).Sum();
+                }
+
+                return _total;
+            }
+
+            set => _total = value;
+        }
     }
 }
