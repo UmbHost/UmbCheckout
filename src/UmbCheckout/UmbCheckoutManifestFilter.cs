a2bf9f14055aa6d96585bfbdcf4dccb4814cbce0
Aaron <aaron@umbhost.net>
2023-08-25 10:39:10 +0100

    Added manifest filter

---
+++ src/UmbCheckout/UmbCheckoutManifestFilter.cs
@@ -0,0 +1,31 @@
+﻿using UmbCheckout.Shared;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Cms.Core.Manifest;
+
+namespace UmbCheckout
+{
+    public class UmbCheckoutManifest : IComposer
+    {
+        public void Compose(IUmbracoBuilder builder)
+        {
+            builder.ManifestFilters().Append<UmbCheckoutManifestFilter>();
+        }
+    }
+
+    public class UmbCheckoutManifestFilter : IManifestFilter
+    {
+        public void Filter(List<PackageManifest> manifests)
+        {
+            var assembly = typeof(UmbCheckoutManifestFilter).Assembly;
+
+            manifests.Add(new PackageManifest
+            {
+                PackageName = Consts.PackageName,
+                Version = assembly.GetName().Version.ToString(3),
+                AllowPackageTelemetry = true,
+                BundleOptions = BundleOptions.None
+            });
+        }
+    }
+}
