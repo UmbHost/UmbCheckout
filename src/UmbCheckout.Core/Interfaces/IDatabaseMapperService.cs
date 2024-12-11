15ecb7838a9a0f357b024a8a8a272139f7006c61
Aaron Sadler <aaron@umbhost.net>
2024-12-11 10:27:37 +0000

    Added database mapping service

---
+++ src/UmbCheckout.Core/Interfaces/IDatabaseMapperService.cs
@@ -0,0 +1,18 @@
+﻿using UmbCheckout.Core.Pocos;
+using UmbCheckout.Shared.Models;
+
+namespace UmbCheckout.Core.Interfaces
+{
+    /// <summary>
+    /// A service which handles the mapping the basket to and from the database
+    /// </summary>
+    public interface IDatabaseMapperService
+    {
+        /// <summary>
+        /// Converts a UmbCheckoutBasket to a Basket
+        /// </summary>
+        /// <param name="umbCheckoutBasket">The UmbCheckoutBasket to be converted</param>
+        /// <returns>Basket conversion</returns>
+        Basket? ToBasket(UmbCheckoutBasket umbCheckoutBasket);
+    }
+}
