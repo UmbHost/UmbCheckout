a0824a9a8b004e6ff3f29d22a4df1b4d53907fea
Kyle Eck <eckkyle2@gmail.com>
2025-01-15 07:29:08 -0500

    add additional notification handlers for the provider

---
+++ src/UmbCheckout.Shared/Notifications/PaymentProvider/OnProviderSessionOptionsAfterFullyCreated.cs
@@ -0,0 +1,18 @@
+﻿using Stripe.Checkout;
+using Umbraco.Cms.Core.Notifications;
+
+namespace UmbCheckout.Stripe.Services
+{
+    /// <summary>
+    ///     Notification which is triggered just after the session options have been fully created with all options
+    /// </summary>
+    public class OnProviderSessionOptionsAfterFullyCreated : INotification
+    {
+        public SessionCreateOptions Options { get; set; }
+
+        public OnProviderSessionOptionsAfterFullyCreated(SessionCreateOptions options)
+        {
+            Options = options;
+        }
+    }
+}
---
+++ src/UmbCheckout.Shared/Notifications/PaymentProvider/OnProviderSessionOptionsLineItemAddedBefore.cs
@@ -0,0 +1,19 @@
+﻿using Stripe.Checkout;
+using Umbraco.Cms.Core.Notifications;
+
+namespace UmbCheckout.Stripe.Services
+{
+    /// <summary>
+    ///     Notification which is triggered just before a stripe line item is added to the
+    ///     line item collection that is sent to Stripe
+    /// </summary>
+    public class OnProviderSessionOptionsLineItemAddedBefore : INotification
+    {
+        public SessionLineItemOptions LineItem { get; set; }
+
+        public OnProviderSessionOptionsLineItemAddedBefore(SessionLineItemOptions lineItem)
+        {
+            LineItem = lineItem;
+        }
+    }
+}
